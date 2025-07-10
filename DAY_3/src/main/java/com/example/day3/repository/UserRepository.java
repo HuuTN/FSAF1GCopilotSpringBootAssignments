b622497fbecf0e3a2a9288741e8f392a092a0ea4
HuuTN <huutn@fsoft.com.vn>
2025-07-10 13:42:28 +0700

    clean

--- DAY_3/src/main/java/com/example/day3/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.day3.repository;
-
-import com.example.day3.model.UserEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<UserEntity, Long> {
-    // You can add custom query methods here if needed
-}
